0cea948187f8d1964862380beda3403b7da80012
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2017-10-19 22:32:35 -0500

    WIP new build proces

--- build.py
+++ build.py
@@ -5,18 +5,19 @@ import re
 
 def update_file(filename):
 
-    f = open(filename, 'r')
-
-    content = f.read()
-    f.close()
-    f = open(filename, 'w')
-
-    content = re.sub('id = sai','id = slack', content)
-    content = re.sub('[sai]','[slack]', content)
-    content = re.sub('action.sai','action.slack', content)
-
-    f.write(content)
-    f.close()
+    print filename
+    # f = open(filename, 'r')
+    #
+    # content = f.read()
+    # f.close()
+    # f = open(filename, 'w')
+    #
+    # content = re.sub('id = sai','id = slack', content)
+    # content = re.sub('[sai]','[slack]', content)
+    # content = re.sub('action.sai','action.slack', content)
+    #
+    # f.write(content)
+    # f.close()
 
     return
 

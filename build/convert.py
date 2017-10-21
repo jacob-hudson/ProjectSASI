a08a8b0b4e7e627c3ab064ab7486d416436b1a89
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2017-10-20 23:04:51 -0500

    new dir structure

--- build/convert.py
+++ build/convert.py
@@ -9,13 +9,13 @@ def update_file(filename):
     f.close()
     f = open(filename, 'w')
 
-    if filename == '../default/alert_actions.conf':
+    if filename == './sai/default/alert_actions.conf':
         content = re.sub('\[sai\]','[slack]', content)
-    elif filename == '../default/app.conf':
+    elif filename == './sai/default/app.conf':
         content = re.sub('id = sai','id = slack', content)
-    elif filename == '../default/restmap.conf' or filename == '../default/data/ui/alerts/sai.html':
+    elif filename == './sai/default/restmap.conf' or filename == './sai/default/data/ui/alerts/sai.html':
         content = re.sub('action.sai','action.slack', content)
-    elif filename == '../default/setup.xml':
+    elif filename == './sai/default/setup.xml':
         content = re.sub('sai','slack', content)
     else:
         pass
@@ -27,20 +27,21 @@ def update_file(filename):
 
 def traverse_dirs(cwd):
     excluded_files = ['build.py', 'build_all.sh', '.travis.yml', 'README.md']
-    for root, _, filenames in os.walk('../'):
+    for root, _, filenames in os.walk('./sai/'):
         for filename in filenames:
             if filename in excluded_files or os.path.join(root,filename).split('/')[1] == ".git":
                 continue
             else:
                 update_file(os.path.join(root,filename))
-                if os.path.join(root,filename) == '../bin/sai.py' or os.path.join(root,filename) == '../default/data/ui/alerts/sai.html':
-                    if os.path.join(root,filename) == '../bin/sai.py':
-                         os.rename('../bin/sai.py','../bin/slack.py')
+                if os.path.join(root,filename) == './sai/bin/sai.py' or os.path.join(root,filename) == './sai/default/data/ui/alerts/sai.html':
+                    if os.path.join(root,filename) == './sai/bin/sai.py':
+                         os.rename('./sai/bin/sai.py','./sai/bin/slack.py')
                     else:
-                         os.rename('../default/data/ui/alerts/sai.html','../default/data/ui/alerts/slack.html')
+                         os.rename('./sai/default/data/ui/alerts/sai.html','./sai/default/data/ui/alerts/slack.html')
 
 def main():
     traverse_dirs('.')
+    os.rename('sai','slack')
 
 
 if __name__ == '__main__':
